0ff19fae8523a94ce84948084f3d234f6f19e96a
Alex <alexlegosergeev@yandex.ru>
2026-02-16 21:54:22 +0300

    feature/BA-16: Настроил алиасы путей импорта через Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
   const isDev = mode === 'development'
 
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+        "@components": resolve(__dirname, "src/components"), 
+      },
+    },
     esbuild: {
       loader: 'jsx',
       include: /src\/.*\.jsx?$/,
